ffda1babd1a7c1970e0d7d377e8689ca65a1b324
Jihan Ahmed <jihanahmed41@gmail.com>
2023-11-28 11:46:33 -0800

    adding file no. 102

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,7 +2,6 @@
 def magic_calculation(a, b, c):
     if (b < a):
         return (c)
-    elif (b > c):
+    if (b > c):
         return (a + b)
-    else:
-        return (a * b - c)
+    return (a * b - c)
