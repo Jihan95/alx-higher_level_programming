4b16467dfbe736e1e23672a81f8eabd0cbfc2ac5
Jihan Ahmed <jihanahmed41@gmail.com>
2023-11-30 04:33:25 -0800

    adding file no.2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,4 +6,4 @@ if (__name__ == "__main__"):
     else:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
