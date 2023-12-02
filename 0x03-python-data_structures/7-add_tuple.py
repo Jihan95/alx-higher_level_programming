ce786fd30a9eedbd6433f037fc03b49e52f2adb5
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-02 04:26:26 -0800

    updating file no.7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,4 +10,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_t = tuple_a[0] + tuple_b[0], tuple_a[1]
     elif (len(tuple_a) >= 2 and len(tuple_b) == 0):
         new_t = tuple_a
+    else:
+        new_t = 0, 0
     return(new_t)
