bfeac01cb23a008ff7b14596cf650402c51284fe
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-06 13:28:24 -0800

    adding file no.7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = a_dictionary
-    new_dic[key] = value
-    return(new_dic)
+    if (a_dictionary is not None):
+        new_dic = a_dictionary
+        new_dic[key] = value
+        return(new_dic)
