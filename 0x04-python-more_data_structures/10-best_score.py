c0aea65ad10f1dd80f446deecab76a8685ea991b
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-06 14:13:35 -0800

    updating file no.10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,5 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return(None)
-    mkey = next(iter(a_dictionary), None)
-    n = a_dictionary[mkey]
-    for key in a_dictionary:
-        if (a_dictionary[key] > n):
-            mkey = key
-    return(mkey)
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return(max_key)
