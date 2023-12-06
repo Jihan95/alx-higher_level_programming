37d64e43ef3b9f6e7d17462e044b541b63c8c0ac
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-06 09:51:46 -0800

    adding file no.1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if (my_list is None):
+        return
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if (my_list[i] == search):
+            new_list[i] = replace
+    return (new_list)
