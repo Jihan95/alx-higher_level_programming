01c3e7c0a0479e523062e51f8c7e4ffe9604ccb5
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-11 05:13:24 -0800

    adding file no.100@

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if (my_list is None):
+    if (my_list is None or len(my_list) == 0):
         return 0
     total_sum = 0
     total_mul = 0
