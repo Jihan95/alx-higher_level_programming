45fa9b28d9d828cd83ab558b5357a3641cdcef7c
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-18 05:19:45 -0800

    updating file 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1,4):
+    for i in range(1, 4):
         try:
             if (i > a):
                 raise Exception('Toofar')
             else:
                 result += (a ** b) / i
-        except:
+        except Toofar:
             result = a + b
             break
     return result
