4a3c8b3a23e56133a8df01e34ff7fbe851e60a37
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-19 05:25:21 -0800

    modifying file no.0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,19 @@
 
 
 class Square:
-    """An empty class Square."""
+    """A class representing a square.
 
+    This class is currently empty and serves as a placeholder for potential
+    future functionality related to squares.
 
+    Note:
+        At the moment, the class does not have any attributes or methods.
+
+    Examples:
+        This class can be used as a base for creating specific types of squares
+        with additional functionality.
+
+    TODO:
+        Add attributes and methods as needed for specific use cases.
+    """
     pass
