94f0f46927a68a3bc158ecc6b93c99fd9698cc60
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-19 07:35:16 -0800

    updating file no.6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -90,8 +90,9 @@ class Square:
         """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(' ' * self.__position[0], end='')
-            print('#' * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
