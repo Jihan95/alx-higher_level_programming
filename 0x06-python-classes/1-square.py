3b3aac3783d705fab30436c5c0e4b630d6dc838b
Jihan Ahmed <jihanahmed41@gmail.com>
2023-12-19 05:46:53 -0800

    adding file no.1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module has a class definition"""
+
+
+class Square:
+    """A class representing a square.
+
+    This class is serving a functionality related to squares.
+
+    Attributes:
+    __size (int): the size of the square
+    """
+    def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
