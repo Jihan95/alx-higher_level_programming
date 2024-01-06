e905566a99fe63cc71cb20044abef08051836ac2
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-06 14:04:10 -0800

    updatind testing file for max_int function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,23 +5,47 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """
+    class to test function max_integer
+    """
     def test_normal_list(self):
+        """
+        Function to test the max_integer function
+        normal positive numbers
+        """
         lis = [5, 6, 10, 80, 3]
         self.assertEqual(max_integer(lis), max(lis))
 
     def test_list_negative_num(self):
+        """
+        Function to test the max_integer function
+        in case of negative numbers
+        """
         lis = [-5, -2, -10, -6]
         self.assertEqual(max_integer(lis), max(lis))
 
     def test_mixed_list(self):
+        """
+        Function to test max_integer function
+        mixed types input
+        """
         lis = [-1, 0, -8, 8, -2]
         self.assertEqual(max_integer(lis), max(lis))
 
     def test_none_list(self):
+        """
+        Function to test max_integer function
+        in case of input is None
+        """
         lis = None
         self.assertEqual(max_integer(lis), None)
 
     def test_not_list(self):
+        """
+        Function to test max_integer function
+        in case of input is not alist or containing 
+        non numbers elements
+        """
         not_list = (5, 10)
         lis = ['cat', '1', '2']
         self.assertRaises(TypeError, max_integer, not_list)
