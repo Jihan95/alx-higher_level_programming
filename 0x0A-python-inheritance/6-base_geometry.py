74088c4bf9799680b6e2e8e49c6c05fe19350332
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-08 05:14:15 -0800

    adding file no.6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that contain an empty class
+"""
+
+
+class BaseGeometry():
+    """
+    an empty class
+    """
+    def area(self):
+        """
+        unimplemented function
+        """
+        raise Exception("area() is not implemented")
