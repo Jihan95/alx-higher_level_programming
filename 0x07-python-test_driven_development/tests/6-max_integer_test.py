505d9e554c24667b496c9a4e05520eedf5118397
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-07 23:11:20 -0800

    solving fatal errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,7 @@
 """Unittest for max_integer([..])
 """
 import unittest
-max_integer = __import__('6-max_integer').max_integeri
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
