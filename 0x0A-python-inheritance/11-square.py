4fc216698ee8da26c26e63ab5a5ca918fc467369
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-08 08:17:34 -0800

    adding file no.11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+Module that contain an BaseGeometry class
+and a recatangle class
+"""
+
+
+class BaseGeometry():
+    """
+    an abstract class for any geometry shape
+    """
+    def area(self):
+        """
+        unimplemented function
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Function to validate an integer
+
+        Args:
+        - name (string): the name of geometry
+        - value (int): value to be validated
+
+        Raises:
+        TypeError: if value isn't an integer
+        ValueError: if value is less than or equal 0
+        """
+        if type(name) != str:
+            raise TypeError("{} must be a string".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class that represents a rectangle
+
+    Attributes:
+    -height (int): the height of the rectangle
+    -width (int): the width of the rectangle
+    """
+    def __init__(self, width, height):
+        """
+        Fanction that instantiate a Rectangle object
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Function to calculate rectangle area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        retrun the rectangle in shape:
+        [Rectangle] <width>/<height>
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """
+    A class that represents a square
+
+    Attributes:
+    __size (int): the length of the square
+    """
+    def __init__(self, size):
+        """
+        Function that intantiate a square object
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Function to return the area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        String representation of the square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
