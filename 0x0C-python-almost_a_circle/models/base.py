83f5d5b5407b9d71ec5b91300fb99a702f259d33
Jihan Ahmed <jihanahmed41@gmail.com>
2024-01-15 09:49:03 -0800

    updating 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,7 @@ class Base:
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
-        return str(list_dictionaries)
+        return dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
