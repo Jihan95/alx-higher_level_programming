8b1da0c4ab4f9dfd7a1114f287dfe37534c9399a
Jihan <jihanahmed41@gmail.com>
2024-02-13 10:50:09 -0800

    updating file no.11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- a script that lists all records with a score >= 10 in the table second_table
-SELECT score, name from second_table WHERE score >= 10
-ORDER BY score DESC;
+SELECT score, name from second_table WHERE score >= 10 AND ORDER BY score DESC;
