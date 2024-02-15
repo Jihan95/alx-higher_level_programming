9c72a6db33fbb896afd9f9e1d57c0141a9f9d3e7
Jihan <jihanahmed41@gmail.com>
2024-02-15 04:03:39 -0800

    updating 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,3 @@
 --  a script that lists all shows contained in the database
-SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_generes.show_id
+SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_generes.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
