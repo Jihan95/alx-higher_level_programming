81c93c2cf0152e5f9938facc912e414110edc32c
Jihan <jihanahmed41@gmail.com>
2024-02-15 02:30:56 -0800

    adding 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
-SELECT id,name FROM cities
-LEFT JOIN states ON state_id=id ORDER BY id;
+SELECT cities.id,cities.name,states.name FROM cities
+LEFT JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
