f8d39dab3e3c195756e3e0a84ee9dd1b3b853cff
jihan <jihanahmed41"gmail.com>
2024-03-12 20:05:58 +0100

    adding squares

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) { process.stdout.write(c); }
+        console.log();
+      }
+    } else { this.print(); }
+  }
+}
+
+module.exports = Square;
