29c7c292ffda4b591a5ec8003c876a7c7f67dd75
Jihan <jihanahmed41@gmail.com>
2024-03-15 00:14:30 -0700

    updating 1, 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,18 +7,19 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3])
-    cursor = connection.cursor()
-    cursor.execute("""
-    SELECT id, name from states WHERE name LIKE 'N%' ORDER BY id ASC
-    """)
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-    cursor.close()
-    connection.close()
+    if len(sys.argv) > 3:
+        connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3])
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT id, name from states WHERE name LIKE 'N%' ORDER BY id ASC
+        """)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        connection.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,17 +7,18 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3])
-    cursor = connection.cursor()
-    query = "SELECT id, name FROM states WHERE name = '{}'"
-    cursor.execute(query.format(sys.argv[4]))
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-    cursor.close()
-    connection.close()
+    if len(sys.argv) > 3:
+        connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3])
+        cursor = connection.cursor()
+        query = "SELECT id, name FROM states WHERE name = '{}'"
+        cursor.execute(query.format(sys.argv[4]))
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        connection.close()
