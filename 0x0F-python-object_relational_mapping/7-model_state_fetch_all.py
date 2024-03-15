5e0d11b1ff9976cc68d23d4d50584e57eb2ec39c
Jihan <jihanahmed41@gmail.com>
2024-03-15 03:43:29 -0700

    updating file 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,21 +2,23 @@
 """
 Module that lists all State objects from the database hbtn_0e_6_usa
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-first = sys.argv[1]
-second = sys.argv[2]
-third = sys.argv[3]
-en = 'mysql+mysqldb://{}:{}@localhost/{}'.format(first, second, third)
-engine = create_engine(en, pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
 
-states = session.query(State).order_by(State.id)
+    first = sys.argv[1]
+    second = sys.argv[2]
+    third = sys.argv[3]
+    en = 'mysql+mysqldb://{}:{}@localhost/{}'.format(first, second, third)
+    engine = create_engine(en, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for row in states:
-    print("{}: {}".format(row.id, row.name))
+    states = session.query(State).order_by(State.id)
+
+    for row in states:
+        print("{}: {}".format(row.id, row.name))
