6003474ac6e7120ae2ec8dc0a2b4b60a10971821
Jihan <jihanahmed41@gmail.com>
2024-03-15 06:31:02 -0700

    updating file 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id==2).first()
+    state = session.query(State).filter(State.id == 2).first()
 
     if state:
         state.name = 'New Mexico'
