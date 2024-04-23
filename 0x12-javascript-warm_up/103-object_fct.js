31a66b127ad9b19bdf8d63f4ce5341552680af12
jihan <jihanahmed41"gmail.com>
2024-04-23 15:05:48 +0200

    updating 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value += 1;
-    return this;
-  }
+  value: 12
 };
+
+myObject.incr = function () {
+  this.value += 1;
+  return this;
+};
+
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
