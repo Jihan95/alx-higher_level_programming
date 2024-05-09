60294c60d57e4be1184a428a4ca49538f757736f
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-09 14:37:31 -0700

    adding files 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,6 @@
 
 def find_peak(list_of_integers):
     """ Function to find the peak at list of numbers """
-    if not list_of_integers:
+    if not list_of_integers or not type(list_of_integers, list):
         return None
     return max(list_of_integers)
