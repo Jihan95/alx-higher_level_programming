97af30f9dd139c00d0a5c6d900285b784defcbe4
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-09 14:32:02 -0700

    adding files 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Function to find the peak at list of numbers """
+def find_peak(list_of_integers):
+    """ Function to find the peak at list of numbers """
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
