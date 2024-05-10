44d0f94e013a8e8d04430d2808a46a33d62d26e6
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-10 04:26:52 -0700

    updating file 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,15 @@
  a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-print('Body response:')
-print('    - type: ', end='')
-print(type(html))
-print('    - content: ', end='')
-print(html)
-print('    - utf8 content: ', end='')
-encoding = response.headers.get_content_charset()
-if encoding and 'utf-8' in encoding.lower():
-    print('OK')
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    print('Body response:')
+    print('    - type: ', end='')
+    print(type(html))
+    print('    - content: ', end='')
+    print(html)
+    print('    - utf8 content: ', end='')
+    print(html.decode('utf-8'))
