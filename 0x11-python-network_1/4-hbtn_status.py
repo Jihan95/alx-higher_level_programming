fbb6979bbf3f527c3222c11bd76a89586b4017a8
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-12 04:00:09 -0700

    updating file 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type : ", end='')
+    print("\t- type: ", end='')
     print(type(response.text))
-    print("\t- content : ", end='')
+    print("\t- content: ", end='')
     print(response.text)
