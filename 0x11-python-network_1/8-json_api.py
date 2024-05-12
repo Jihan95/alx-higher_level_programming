eb89072dd8285f9898946537c808f978e911d0e6
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-12 05:07:25 -0700

    adding file 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        data = {'q': ""}
+    else:
+        data = {'q': sys.argv[1]}
+    response = requests.post(url, data)
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data[id], json_data[name]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
