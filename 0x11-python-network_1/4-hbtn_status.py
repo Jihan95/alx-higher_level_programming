e1410b2f082459b1e10119d47ace71f3a1032133
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-12 03:49:34 -0700

    adding file 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# a Python script that fetches https://alx-intranet.hbtn.io/status
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type : ", end='')
+    print(type(response.text))
+    if response.ok:
+        print("\t- content : OK")
