acd105618b21b96c3f02da72c7580ebbea181473
Jihan Ahmed <jihanahmed41@gmail.com>
2024-05-12 05:10:26 -0700

    adding file 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     try:
         json_data = response.json()
         if json_data:
-            print("[{}] {}".format(json_data[id], json_data[name]))
+            print("[{}] {}".format(json_data["id"], json_data["name"]))
         else:
             print("No result")
     except ValueError:
