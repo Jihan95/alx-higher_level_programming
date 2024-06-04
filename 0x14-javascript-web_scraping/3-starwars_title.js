021d4c83e2caceb0fb75457fe40f5de9a3452c5b
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-04 05:53:14 -0700

    adding file 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const request = require('request');
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(JSON.parse(body).title);
+});
