52dba42cfb996d555eb1cfcac6d1227004067bf4
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-04 05:46:12 -0700

    updating file 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,11 +2,7 @@
 const url = process.argv[2];
 const request = require('request');
 
-const options = {
-  url: url,
-  method: 'GET'
-};
-request(options, (err, res, body) => {
+request(url, (err, res) => {
   if (err) {
     console.log(err);
   }
