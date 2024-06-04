4472fe8d0756d7a74c4e49e2fa7bd755b91c1343
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-04 05:31:10 -0700

    adding file 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+
+const options = {
+  url: url,
+  method: 'GET'
+};
+request(options, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('code', res.statusCode);
+});
