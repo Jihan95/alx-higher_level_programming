695d671935ba93739bf43363c87ec8f9cc67c7ec
Jihan Ahmed <jihanahmed41@gmail.com>
2024-06-04 05:22:16 -0700

    update file 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const fs = require('fs');
-const text = args[1] + '\n';
+const text = args[1];
 
 fs.writeFile(args[0], text, 'utf8', (err) => {
   if (err) {
